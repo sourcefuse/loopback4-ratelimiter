e05f059b680dfb30994d6689ca1b0e599262d672
Samarpan <samarpan.bhattacharya@sourcefuse.com>
2021-11-08 17:43:27 +0530

    fix(ci-cd): remove ticket id from being mandatory

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,6 @@ module.exports = {
     'header-max-length': [2, 'always', 100],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [0, 'always'],
-    'references-empty': [2, 'never'],
   },
   parserPreset: {
     parserOpts: {
