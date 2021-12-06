24cc01abb63000d35df64ac9a23e74ae8c52a708
Ankur Bansal <85155003+AnkurBansalSF@users.noreply.github.com>
2021-12-06 18:19:18 +0530

    fix(providers): windowMs property was not working inside decorator (#32)

--- src/providers/ratelimit-datasource.provider.ts
+++ src/providers/ratelimit-datasource.provider.ts
@@ -1,5 +1,6 @@
 import {CoreBindings, inject, Provider} from '@loopback/core';
-import {RateLimitOptions} from '../types';
+import {Getter} from '@loopback/repository';
+import {RateLimitMetadata, RateLimitOptions} from '../types';
 import {RateLimitSecurityBindings} from '../keys';
 import {Store} from 'express-rate-limit';
 import RedisStore from 'rate-limit-redis';
@@ -12,23 +13,37 @@ export class RatelimitDatasourceProvider
   implements Provider<Store | MemcachedStore | undefined>
 {
   constructor(
+    @inject.getter(RateLimitSecurityBindings.METADATA)
+    private readonly getMetadata: Getter<RateLimitMetadata>,
     @inject(CoreBindings.APPLICATION_INSTANCE)
     private readonly application: RestApplication,
     @inject(RateLimitSecurityBindings.CONFIG, {optional: true})
     private readonly config?: RateLimitOptions,
   ) {}
 
-  async value(): Promise<Store | MemcachedStore | undefined> {
+  value(): Promise<Store | MemcachedStore | undefined> {
+    return this.action();
+  }
+
+  async action(): Promise<Store | MemcachedStore | undefined> {
+    const metadata: RateLimitMetadata = await this.getMetadata();
+
+    // First check if rate limit options available at method level
+    const operationMetadata = metadata ? metadata.options : {};
+
+    // Create options based on global config and method level config
+    const opts = Object.assign({}, this.config, operationMetadata);
+
     if (this.config?.type === 'MemcachedStore') {
       return new MemcachedStore({
         client: this.config?.client,
-        expiration: (this.config?.windowMs ?? 60 * 1000) / 1000,
+        expiration: (opts.windowMs ?? 60 * 1000) / 1000,
       });
     } else if (this.config?.type === 'MongoStore') {
       return new MongoStore({
         uri: this.config?.uri,
         collectionName: this.config?.collectionName,
-        expireTimeMs: (this.config?.windowMs ?? 60 * 1000) / 1000,
+        expireTimeMs: (opts.windowMs ?? 60 * 1000) / 1000,
       });
     } else {
       const redisDS = (await this.application.get(
@@ -37,7 +52,7 @@ export class RatelimitDatasourceProvider
       if (redisDS?.connector) {
         return new RedisStore({
           client: redisDS.connector._client,
-          expiry: (this.config?.windowMs ?? 60 * 1000) / 1000,
+          expiry: (opts.windowMs ?? 60 * 1000) / 1000,
         });
       } else {
         throw new HttpErrors.InternalServerError('Invalid Datasource');
